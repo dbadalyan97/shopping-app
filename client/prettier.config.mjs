bb10e453c186f39745ba4ceba2b2c2d237eb8322
dbadalyan97 <badalyan9779@gmail.com>
2026-07-23 11:57:17 +0400

    chore: add Prettier and tighten TypeScript and ESLint tooling

---
+++ client/prettier.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import("prettier").Config} */
+const config = {
+  semi: true,
+  singleQuote: false,
+  trailingComma: "all",
+  tabWidth: 2,
+  printWidth: 80,
+  endOfLine: "lf",
+  arrowParens: "always",
+  bracketSpacing: true,
+};
+
+export default config;
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,7 +1,3 @@
 export default function Home() {
-  return (
-    <div className="bg-red-500">
-      Homepage
-    </div>
-  );
+  return <div className="bg-red-500">Homepage</div>;
 }
