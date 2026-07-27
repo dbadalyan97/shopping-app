42ec1357be1901c97400329e01744af98b50280a
dbadalyan97 <badalyan9779@gmail.com>
2026-07-27 11:11:49 +0400

    feat: mount Navbar and DictionaryProvider in root layout

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,9 +1,11 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 
-import { getDictionary } from "@/i18n/get-dictionary";
+import { DictionaryProvider } from "@/provider/DictionaryProvider";
+import { getDictionary } from "@/dictionary/get-dictionary";
 
 import "./globals.css";
+import { Navbar } from "@/components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,11 +26,13 @@ export async function generateMetadata(): Promise<Metadata> {
   };
 }
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const dictionary = await getDictionary();
+
   return (
     <html
       lang="en"
@@ -37,9 +41,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="mx-auto p-4 sm:max-w-xl md:max-w-2xl lg:max-w-3xl xl:max-w-6xl">
-          {children}
-        </div>
+        <DictionaryProvider dictionary={dictionary}>
+          <div className="mx-auto p-4 sm:max-w-xl md:max-w-2xl lg:max-w-3xl xl:max-w-6xl">
+            <Navbar />
+            {children}
+          </div>
+        </DictionaryProvider>
       </body>
     </html>
   );
