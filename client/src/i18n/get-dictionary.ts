c2a445d15d8a98e7a8217cbad9502b77d18f0761
dbadalyan97 <badalyan9779@gmail.com>
2026-07-27 11:10:34 +0400

    chore: remove leftover i18n module after dictionary move

--- client/src/i18n/get-dictionary.ts
+++
@@ -1,8 +0,0 @@
-import "server-only";
-
-const dictionary = () =>
-  import("./dictionaries/en.json").then((module) => module.default);
-
-export type Dictionary = Awaited<ReturnType<typeof dictionary>>;
-
-export const getDictionary = async (): Promise<Dictionary> => dictionary();
