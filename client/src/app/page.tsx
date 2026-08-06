aec9b74aea08993461e7021bbbb915e50b33f4d9
dbadalyan97 <badalyan9779@gmail.com>
2026-08-06 15:25:35 +0400

    feat: mount featured image and ProductList on home page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,12 +1,19 @@
-import { getDictionary } from "@/dictionary/get-dictionary";
+import { ProductList } from "@/components/Product/ProductList";
+import Image from "next/image";
 
 export default async function Home() {
-  const dictionary = await getDictionary();
-
   return (
-    <div>
-      <h1 className="text-2xl font-semibold">{dictionary.home.title}</h1>
-      <p className="mt-2 text-neutral-600">{dictionary.home.subtitle}</p>
+    <div className="">
+      <div className="relative aspect-3/1 mb-12">
+        <Image
+          src="/featured.png"
+          alt="Featured Product"
+          fill
+          loading="lazy"
+          sizes="(max-width: 768px) 100vw, (max-width: 1024px)"
+        />
+      </div>
+      <ProductList />
     </div>
   );
 }
