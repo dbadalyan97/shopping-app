3293ab81adbcc8c978e9836866033da353b2b05a
dbadalyan97 <badalyan9779@gmail.com>
2026-08-26 23:12:38 +0400

    feat: git add client/src/types/product.ts

--- client/src/types/product.ts
+++ client/src/types/product.ts
@@ -1,13 +1,14 @@
 export type ProductColor = string;
 export type ProductColors = [ProductColor, ...ProductColor[]];
-
+export type ProductSize = string;
+export type ProductSizes = [ProductSize, ...ProductSize[]];
 export interface ProductType {
   id: number | string;
   name: string;
   shortDescription: string;
   description: string;
   price: number;
-  sizes: string[];
+  sizes: ProductSizes;
   colors: ProductColors;
   images: Record<ProductColor, string>;
 }
