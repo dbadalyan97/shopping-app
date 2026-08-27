1f2a75dedb0b34e3eedde0ea3f67d4498afe93ef
dbadalyan97 <badalyan9779@gmail.com>
2026-08-27 21:06:13 +0400

    feat: add persist

--- client/src/store/cart/cartStore.ts
+++ client/src/store/cart/cartStore.ts
@@ -1,18 +1,26 @@
 import { create } from "zustand";
 import { CartStoreActionType, CartStoreStateType } from "./types";
 import { CartItemType } from "@/types/cart";
+import { createJSONStorage } from "zustand/middleware";
+import { persist } from "zustand/middleware";
 
-const useCartStore = create<CartStoreStateType & CartStoreActionType>(
-  (set) => ({
-    cart: [],
-    addToCart: (product: CartItemType) =>
-      set((state) => ({ cart: [...state.cart, product] })),
-    removeFromCart: (productId: CartItemType["id"]) =>
-      set((state) => ({
-        cart: state.cart.filter((item) => item.id !== productId),
-      })),
-    clearCart: () => set({ cart: [] }),
-  }),
+const useCartStore = create<CartStoreStateType & CartStoreActionType>()(
+  persist(
+    (set) => ({
+      cart: [],
+      addToCart: (product: CartItemType) =>
+        set((state) => ({ cart: [...state.cart, product] })),
+      removeFromCart: (productId: CartItemType["id"]) =>
+        set((state) => ({
+          cart: state.cart.filter((item) => item.id !== productId),
+        })),
+      clearCart: () => set({ cart: [] }),
+    }),
+    {
+      name: "cart",
+      storage: createJSONStorage(() => localStorage),
+    },
+  ),
 );
 
 export default useCartStore;
